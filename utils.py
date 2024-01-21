c3625874e6cb894d976af2f6234a80064205a11d
francisco melazzini <francisco.melazzini@physics.msu.ru>
2024-01-21 18:41:32 +0300

    created the DL model, loss fn, optimizer and timing code

--- utils.py
+++ utils.py
@@ -541,3 +541,19 @@ class NormalizedSpectrumInfo:
             raise RuntimeError("the left column density interval is not valid!")
         if abs(1/right-1/RIGHT_NH)>DEFAULT_EPSILON:
             raise RuntimeError("the right column density interval is not valid!")
+
+
+def print_train_time(start, end, device=None):
+    """Prints difference between start and end time.
+
+    Args:
+        start (float): Start time of computation (preferred in timeit format). 
+        end (float): End time of computation.
+        device ([type], optional): Device that compute is running on. Defaults to None.
+
+    Returns:
+        float: time between start and end in seconds (higher is longer).
+    """
+    total_time = end - start
+    print(f"\nTrain time on {device}: {total_time:.3f} seconds")
+    return total_time
